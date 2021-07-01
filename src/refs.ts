a33dc3bfd22eca406a7fd31b5c70f27c58be3959
maiijy <maiijy@126.com>
2021-07-01 19:06:29 +0800

    fix: 修改配置文件名

--- src/refs.ts
+++ src/refs.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { ICssVarRefs } from './types';
 
-const CSS_VAR_REF_FILE = './theme/css-var-ref.json';
+const CSS_VAR_REF_FILE = 'theme-css-var-refs.json';
 
 export const getZentThemeRefs: () => ICssVarRefs | null = () => {
   // doesn't in node
@@ -19,7 +19,7 @@ export const getZentThemeRefs: () => ICssVarRefs | null = () => {
 
   // zent >= 9.8.1
   try {
-    const refsPath = path.resolve(zentPath.replace("/index.js", ""), CSS_VAR_REF_FILE);
+    const refsPath = path.resolve(zentPath.replace("index.js", ""), CSS_VAR_REF_FILE);
     const data = fs.readFileSync(refsPath, { encoding: 'utf-8' }).toString();
     const refs = JSON.parse(data);
     return refs;
